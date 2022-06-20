1eeee02f4b537bff0a431f1c4027a9cd38ee3bc7
Eunbin <kartrider_12@naver.com>
2022-06-20 23:51:49 +0900

    aop init

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("io.github.microutils:kotlin-logging:2.1.23")
